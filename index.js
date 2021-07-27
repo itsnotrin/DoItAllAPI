a31dfd1ca756ecd3039b32702998c6d5cfa1caed
itsnotrin <30768410+itsnotrin@users.noreply.github.com>
2021-07-27 16:20:25 +0100

    Fixes (ish)

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use('/api/v1/dev', DevRouter)
 
 try{
     app.listen(port, () => {
-        console.log(`Listening on http://localhost:${port}}/api`)
+        console.log(`Listening on http://localhost:${port}/api`)
     })
 }
 catch(err){
--- routes/api/v1/funrouter.js
+++ routes/api/v1/funrouter.js
@@ -1,7 +1,5 @@
 const { Router } = require('express')
-const sqlite3 = require('sqlite3')
-const db = new sqlite3.Database('../../../')
-
+const fs = require("fs");
 const router = Router();
 
 router.get('/', (req, res) => {
@@ -11,6 +9,23 @@ router.get('/', (req, res) => {
 })
 
 router.get('/randomjoke', (req, res) => {
-    return res.send("test")
+function getRandomLine(filename, callback){
+  fs.readFile(filename, "utf-8", function(err, data){
+    if(err) {
+        throw err;
+    }
+
+    // note: this assumes `data` is a string - you may need
+    //       to coerce it - see the comments for an approach
+    var lines = data.split('\n');
+    
+    // choose one of the lines...
+    var line = lines[Math.floor(Math.random()*lines.length)]
+
+    // invoke the callback with our line
+    return line;
+ })
+}
+console.log(getRandomLine('messages/jokes.txt'))
 })
 module.exports = router;
