7703ebbc6af3fcda47d175a4c5639f5e12c3a7e3
itsnotrin <30768410+itsnotrin@users.noreply.github.com>
2021-07-27 15:12:10 +0100

    forgot a bloody curly brace

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use('/api/v1/dev', DevRouter)
 
 try{
     app.listen(port, () => {
-        console.log(`Listening on http://localhost:${process.env.port || 5000}/api`)
+        console.log(`Listening on http://localhost:${process.env.port}}/api`)
     })
 }
 catch(err){
