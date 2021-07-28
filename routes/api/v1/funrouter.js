e1d0702f0d5f9b31efd6078639f0fdffcf9bbccf
itsnotrin <30768410+itsnotrin@users.noreply.github.com>
2021-07-28 12:32:08 +0100

    Cleaned it up so I don't have to type out the func multiple times

--- routes/api/v1/funrouter.js
+++ routes/api/v1/funrouter.js
@@ -2,6 +2,27 @@ const { Router } = require('express')
 const fs = require("fs");
 const router = Router();
 
+function getRandomLine(filename, res){
+    fs.readFile(filename, "utf-8", function(err, data){
+      if(err) {
+          throw err;
+      }
+  
+      // note: this assumes `data` is a string - you may need
+      //       to coerce it - see the comments for an approach
+      var lines = data.split('\n');
+      
+      // choose one of the lines...
+      var line = lines[Math.floor(Math.random()*lines.length)]
+  
+      // invoke the callback with our line
+      return res.send(line);
+      // return console.log(line);
+      
+   })
+  }
+
+
 router.get('/', (req, res) => {
     return res.json({
         "Error": "Missing the request link."
@@ -9,25 +30,6 @@ router.get('/', (req, res) => {
 })
 
 router.get('/randomjoke', (req, res) => {
-function getRandomLine(filename, callback){
-  fs.readFile(filename, "utf-8", function(err, data){
-    if(err) {
-        throw err;
-    }
-
-    // note: this assumes `data` is a string - you may need
-    //       to coerce it - see the comments for an approach
-    var lines = data.split('\n');
-    
-    // choose one of the lines...
-    var line = lines[Math.floor(Math.random()*lines.length)]
-
-    // invoke the callback with our line
-    return res.send(line);
-    // return console.log(line);
-    
- })
-}
-getRandomLine('messages/jokes.txt')
+    getRandomLine('messages/jokes.txt', res)
 })
 module.exports = router;
