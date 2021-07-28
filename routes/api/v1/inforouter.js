9af79ff9a4f312a4c3e86af7bcd03dba8cdf9de3
itsnotrin <30768410+itsnotrin@users.noreply.github.com>
2021-07-28 20:28:32 +0100

    Add emailcheck (kill me please)

--- routes/api/v1/inforouter.js
+++ routes/api/v1/inforouter.js
@@ -1,3 +1,4 @@
+const { default: axios } = require('axios');
 const { Router } = require('express')
 const router = Router();
 
@@ -29,6 +30,38 @@ router.get('/iss-location', (req, res) => {
             }
           })
     })
+})
+
+router.get('/emailcheck' , (req, res) => {
+  //Make a request to the eva email checker API
+  let url = `https://api.eva.pingutil.com/email?email=${req.query.email}`
+  axios.get(url)
+  .then(function (response) {
+    let resp = response["data"]
+    let info = resp["data"]
+    return res.json({
+      "message": "Success",
+      "data": {
+        "email_address": info["email_address"],
+        "domain": info["domain"],
+        "valid_syntax": info["valid_syntax"],
+        "disposable": info["disposable"],
+        "webmail": info["webmail"],
+        "deliverable": info["deliverable"],
+        "gibberish": info["gibberish"],
+        "spam": info["spam"]
+      }
+    }) 
+  })
+  .catch(function (error) {
+    console.log(error)
+    return res.json({
+      "message": "Error",
+      "issue": {
+        "error": error,
+      }
+    })
+  })
 })
 
 module.exports = router;
