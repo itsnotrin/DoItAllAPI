acbcf0a6c6e785cbbcb2410627bb7508c951039a
Ryan Wieczorkiewicz <ryanwfun@gmail.com>
2021-08-02 06:03:13 +0100

    Started work on a frontend before I get too lazy

---
+++ routes/frontend/mainrouter.js
@@ -0,0 +1,16 @@
+const { default: axios } = require('axios');
+const { Router } = require('express')
+const router = Router();
+const path = require('path')
+
+//Add all the endpoints
+router.get('/', (req, res) => {
+    //Uses the current directory and then sends the Static index.html
+    res.sendFile(path.join(__dirname, './Static/html/index.html'));
+});
+
+
+
+
+
+module.exports = router;
