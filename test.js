d1b59ba5f30b3d28a02b0a163ab9e621fe05a5c5
itsnotrin <ryanwfun@gmail.com>
2021-09-01 22:04:50 +0100

    More mental things lmao

--- test.js
+++ test.js
@@ -7,4 +7,5 @@ main.start(3000)
 //Kill the server after 3 seconds with a good exit code, if there's an error, it will auto close with a bad error and fail CI/CD.
 setTimeout((function() {
     return process.exit(0);
+    console.log("Passed the test!")
 }), 3000);
