86ca322f77107ac780cfc75a05ebc0a24832f4a6
itsnotrin <ryanwfun@gmail.com>
2021-09-01 22:06:00 +0100

    Put it on the wrong line

--- test.js
+++ test.js
@@ -6,6 +6,6 @@ main.start(3000)
 
 //Kill the server after 3 seconds with a good exit code, if there's an error, it will auto close with a bad error and fail CI/CD.
 setTimeout((function() {
-    return process.exit(0);
     console.log("Passed the test!")
+    return process.exit(0);
 }), 3000);
