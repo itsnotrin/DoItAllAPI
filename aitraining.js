a4a662257f169486ce8a6224579137c669b91731
itsnotrin <ryanwfun@gmail.com>
2021-09-03 21:41:56 +0100

    Added the aitraining file

---
+++ aitraining.js
@@ -0,0 +1,10 @@
+//Training the AI Using this.
+
+[
+    {phrase: "Joke.", result: {type: 'joke'}},
+    {phrase: "Can you tell me a joke?", result: {type: 'joke'}},
+    {phrase: "Give me a joke?", result: {type: 'joke'}},
+    {phrase: "Fact.", result: {type: 'fact'}},
+    {phrase: "Give me a fact?", result: {type: 'fact'}},
+    {phrase: "Can you tell me a fact?", result: {type: 'fact'}}
+]
